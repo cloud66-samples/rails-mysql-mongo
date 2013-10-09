2eca977f50006dfadb901fe26875709cabd10bcf
Luismi Ramirez <luismir89@gmail.com>
2013-10-09 19:44:21 +0200

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CafretestMysqlMongo::Application.config.secret_token = 'ff6557000610d0602717a62632b256650e27d5aa060306bf6a99758b9ec98a3ea9276a21af387a0d9b002d418e81cf484b1749a3f8974ae527fae68c88fb4555'
